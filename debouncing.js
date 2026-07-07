d623843b50fd908b4d7a80e81cc77dc8ba2961f4
shivam pandey <sp5812070@gmail.com>
2026-07-07 10:35:04 +0530

    test.js

--- debouncing.js
+++ debouncing.js
@@ -18,3 +18,8 @@ debouncedLog('he');
 debouncedLog('hell');
 debouncedLog('hello');
 
+
+
+
+
+
--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 let arr1 = [1,2]
 let arr2 = [...arr1]
 arr2.push(3)
-console.log(arr1)
-console.log(arr2)
+// console.log(arr1)
+// console.log(arr2)
+
+let arr = [1,3,5,7,9,2,4,6,8,10,12,14,16,18,11,13,15,17,19,20]
+
+console.log([...arr].sort(()=>Math.random() - 0.5).slice(0,5))
