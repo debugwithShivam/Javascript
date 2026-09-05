38df8b608203758a128adcf8617a53e6de71eab2
shivam panday <sp5812070@gmail.com>
2026-09-05 08:57:36 +0530

    javascript prototype

--- JSquestion.js
+++ JSquestion.js
@@ -8,10 +8,64 @@
 //     console.log(str)
 // }
 
+
 function loop(n) {
     if (n < 0) return
     console.log(n)
     return loop(n - 1)
 }
 
-console.log(loop(10))
+// console.log(loop(10))
+
+function object(fun,a="8549",b){
+     let obj = {
+        name:"shivam",
+        age:12,
+        class:"12th",
+        roll:"Frontend"
+    }
+    
+    return fun(obj)
+}
+
+const createFunction = (fun) => {
+    object((prev)=>{
+        return fun(prev)
+    })
+}
+
+
+createFunction((a)=>({
+    ...a,
+}))
+
+// console.log(object.prototype)
+// console.log(object.prototype = {name:"snu"})
+// console.log(object.prototype)
+// console.log(object.prototype.name)
+
+let li = [
+    {name:"shivam"},
+    {name:"sonu"},
+    {name:"prince"},
+    {name:"riya"},
+    {name:"harsh"},
+]
+
+
+
+let name = ""
+let list = li.filter((item)=>item.name!==name)
+console.log(list)
+
+let ul = document.querySelector('ul')
+
+
+list.map((item)=>{
+    let liTag = document.createElement('li') 
+    liTag.innerText += item.name
+    ul.append(liTag)
+})
+
+
+
