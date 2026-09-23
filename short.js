bb555c253582f9912ed7d7ebaf17bd7ae4ff7913
shivam panday <sp5812070@gmail.com>
2026-09-23 17:08:10 +0530

    mergeSort & insertionSort algo

--- short.js
+++ short.js
@@ -1,12 +1,17 @@
-const bubbleSort = (array) => {
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = 0; j < array.length - 1 - i; j++) {
-      if (array[j] > array[j + 1]) {
-        [array[j], array[j + 1]] = [array[j + 1], array[j]];
-      }
+function quickSort(arr) {
+  if (arr.length <= 1) return arr; // Base case
+
+  const pivot = arr[arr.length - 1]; // Use last element as pivot
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
     }
   }
-  return array;
-};
 
-module.exports = bubbleSort;
+  return [...quickSort(left), pivot, ...quickSort(right)]; // Recursively combine
+}
